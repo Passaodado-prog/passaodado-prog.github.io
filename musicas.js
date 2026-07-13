ca469dee9e533ce5104e2bba011f54c435ce5a74
Passaodado-prog <neitan693@gmail.comm>
2026-07-13 17:35:14 -0300

    Descrição da alteração

--- musicas.js
+++ musicas.js
@@ -7,7 +7,6 @@ const musicas = [
     artista:"BEATLES",
     ano:1970,
     arquivo:"audio/001.mp3",
-    
 },
 
 {
@@ -17,7 +16,6 @@ const musicas = [
     artista:"CHIC",
     ano:1978,
     arquivo:"audio/002.mp3",
-    
 },
 
 {
@@ -27,7 +25,312 @@ const musicas = [
     artista:"ROBERTA FLACK",
     ano:1973,
     arquivo:"audio/003.mp3",
-    
+},
+
+{
+    id:4,
+    codigo:"004",
+    nome:"Isn't She Lovely",
+    artista:"Stevie Wonder",
+    ano:1976,
+    arquivo:"audio/004.mp3",
+},
+
+{
+    id:5,
+    codigo:"005",
+    nome:"INFINITA HIGHWAY",
+    artista:"ENGENHEIROS DO HAWAII",
+    ano:2004,
+    arquivo:"audio/005.mp3",
+},
+
+{
+    id:6,
+    codigo:"006",
+    nome:"IMAGINE",
+    artista:"JOHN LENNON",
+    ano:1971,
+    arquivo:"audio/006.mp3",
+},
+
+{
+    id:7,
+    codigo:"007",
+    nome:"I'm Coming Out",
+    artista:"DIANA ROSS",
+    ano:1980,
+    arquivo:"audio/007.mp3",
+},
+
+{
+    id:8,
+    codigo:"008",
+    nome:"Have You Ever Seen The Rain",
+    artista:"Creedence Clearwater Revival",
+    ano:1973,
+    arquivo:"audio/008.mp3",
+},
+
+{
+    id:9,
+    codigo:"009",
+    nome:"Gonna Make You Sweat",
+    artista:"C+C Music Factory",
+    ano:1991,
+    arquivo:"audio/009.mp3",
+},
+
+{
+    id:10,
+    codigo:"010",
+    nome:"Footloose",
+    artista:"Kenny Loggins",
+    ano:1984,
+    arquivo:"audio/010.mp3",
+},
+
+{
+    id:11,
+    codigo:"011",
+    nome:"Get Down On It",
+    artista:"Kool & The Gang",
+    ano:1981,
+    arquivo:"audio/011.mp3",
+},
+
+{
+    id:12,
+    codigo:"012",
+    nome:"NUMB",
+    artista:"LINKIN PARK",
+    ano:2003,
+    arquivo:"audio/012.mp3",
+},
+
+{
+    id:13,
+    codigo:"013",
+    nome:"Play That Funky Music",
+    artista:"Wild Cherry",
+    ano:1976,
+    arquivo:"audio/013.mp3",
+},
+
+{
+    id:14,
+    codigo:"014",
+    nome:"A PAZ",
+    artista:"GILBERTO GIL",
+    ano:1994,
+    arquivo:"audio/014.mp3",
+},
+
+{
+    id:15,
+    codigo:"015",
+    nome:"APENAS MAIS UMA DE AMOR",
+    artista:"LULU SANTOS",
+    ano:2000,
+    arquivo:"audio/015.mp3",
+},
+
+{
+    id:16,
+    codigo:"016",
+    nome:"AS QUATRO ESTAÇÕES",
+    artista:"SANDY & JUNIOR",
+    ano:2007,
+    arquivo:"audio/016.mp3",
+},
+
+{
+    id:17,
+    codigo:"017",
+    nome:"BEAT IT",
+    artista:"MICHAEL JACKSON",
+    ano:1982,
+    arquivo:"audio/017.mp3",
+},
+
+{
+    id:18,
+    codigo:"018",
+    nome:"If You Leave Me Now",
+    artista:"CHICAGO",
+    ano:1976,
+    arquivo:"audio/018.mp3",
+},
+
+{
+    id:19,
+    codigo:"019",
+    nome:"Eye of the Tiger",
+    artista:"SURVIVOR",
+    ano:1982,
+    arquivo:"audio/019.mp3",
+},
+
+{
+    id:20,
+    codigo:"020",
+    nome:"All Out Of Love",
+    artista:"Air Supply",
+    ano:1980,
+    arquivo:"audio/020.mp3",
+},
+
+{
+    id:21,
+    codigo:"021",
+    nome:"Celebration",
+    artista:"Kool & The Gang",
+    ano:1980,
+    arquivo:"audio/021.mp3",
+},
+
+{
+    id:22,
+    codigo:"022",
+    nome:"Funkytown",
+    artista:"Lipps Inc.",
+    ano:1980,
+    arquivo:"audio/022.mp3",
+},
+
+{
+    id:23,
+    codigo:"023",
+    nome:"Livin' On A Prayer",
+    artista:"Bon Jovi",
+    ano:1986,
+    arquivo:"audio/023.mp3",
+},
+
+{
+    id:24,
+    codigo:"024",
+    nome:"Malandragem",
+    artista:"Cassia Eller",
+    ano:1994,
+    arquivo:"audio/024.mp3",
+},
+
+{
+    id:25,
+    codigo:"025",
+    nome:"Me Chama",
+    artista:"Lobão",
+    ano:2000,
+    arquivo:"audio/025.mp3",
+},
+
+{
+    id:26,
+    codigo:"026",
+    nome:"Os Cegos do Castelo",
+    artista:"Nando Reis",
+    ano:1997,
+    arquivo:"audio/026.mp3",
+},
+
+{
+    id:27,
+    codigo:"027",
+    nome:"Pescador de Ilusões",
+    artista:"O Rappa",
+    ano:2005,
+    arquivo:"audio/027.mp3",
+},
+
+{
+    id:28,
+    codigo:"028",
+    nome:"Primeiros Erros",
+    artista:"Capital Inicial",
+    ano:2000,
+    arquivo:"audio/028.mp3",
+},
+
+{
+    id:29,
+    codigo:"029",
+    nome:"Rocket Man",
+    artista:"Elton John",
+    ano:1972,
+    arquivo:"audio/029.mp3",
+},
+
+{
+    id:30,
+    codigo:"030",
+    nome:"Sailing",
+    artista:"Christopher Cross",
+    ano:1980,
+    arquivo:"audio/030.mp3",
+},
+
+{
+    id:31,
+    codigo:"031",
+    nome:"September",
+    artista:"Earth, Wind & Fire",
+    ano:1978,
+    arquivo:"audio/031.mp3",
+},
+
+{
+    id:32,
+    codigo:"032",
+    nome:"Starman",
+    artista:"David Bowie",
+    ano:1972,
+    arquivo:"audio/032.mp3",
+},
+
+{
+    id:33,
+    codigo:"033",
+    nome:"Stayin' Alive",
+    artista:"Bee Gees",
+    ano:1977,
+    arquivo:"audio/033.mp3",
+},
+
+{
+    id:34,
+    codigo:"034",
+    nome:"Take On Me",
+    artista:"a-ha",
+    ano:1985,
+    arquivo:"audio/034.mp3",
+},
+
+{
+    id:35,
+    codigo:"035",
+    nome:"I Wanna Dance With Somebody",
+    artista:"Whitney Houston",
+    ano:1987,
+    arquivo:"audio/035.mp3",
+},
+
+{
+    id:36,
+    codigo:"036",
+    nome:"You Give Love a Bad Name",
+    artista:"Bon Jovi",
+    ano:1986,
+    arquivo:"audio/036.mp3",
+},
+
+{
+    id:37,
+    codigo:"037",
+    nome:"You Should Be Dancing",
+    artista:"Bee Gees",
+    ano:1976,
+    arquivo:"audio/037.mp3",
 }
 
 ];
