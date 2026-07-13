0827a3d160e124c48eab58838038e131b69be372
Passaodado-prog <neitan693@gmail.comm>
2026-07-13 15:57:54 -0300

    Descrição da alteração

--- musicas.js
+++ musicas.js
@@ -2,30 +2,30 @@ const musicas = [
 
 {
     id:1,
-    codigo:"POP001",
-    nome:"21 Questions",
-    artista:"50 Cent",
-    ano:2003,
+    codigo:"001",
+    nome:"LET IT BE",
+    artista:"BEATLES",
+    ano:1970,
     arquivo:"audio/001.mp3",
     
 },
 
 {
     id:2,
-    codigo:"POP002",
-    nome:"Evidências",
-    artista:"Chitãozinho & Xororó",
-    ano:1990,
+    codigo:"002",
+    nome:"LE FREAK",
+    artista:"CHIC",
+    ano:1978,
     arquivo:"audio/002.mp3",
     
 },
 
 {
     id:3,
-    codigo:"POP003",
-    nome:"Anna Júlia",
-    artista:"Los Hermanos",
-    ano:1999,
+    codigo:"003",
+    nome:"Killing Me Softly With His Song",
+    artista:"ROBERTA FLACK",
+    ano:1973,
     arquivo:"audio/003.mp3",
     
 }
